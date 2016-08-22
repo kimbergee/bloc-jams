995c2bef1ff294cc137f68d09dac781e0758b732
kimber <kimber.gee@gmail.com>
2016-08-22 16:52:19 -0400

    assignment complete, add event listener for album covers

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumKygo = {
+    title: 'Cloud Nine',
+    artist: 'Kygo',
+    label: 'Sony',
+    year: '2015',
+    albumArtUrl: 'assets/images/album_covers/22.png',
+    songs: [
+        { title: 'Stole the Show', duration: '2:08' },
+        { title: 'Fiction', duration: '4:03' },
+        { title: 'Firestone', duration: '4:33'},
+        { title: 'Not Alone', duration: '3:25' },
+        { title: 'Nothing Left', duration: '3:56'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class="album-view-song-item">'
@@ -40,14 +55,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-     // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
+var setCurrentAlbum = function(album) {
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -62,4 +76,13 @@ var setCurrentAlbum = function(album) {
  
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    var albums = [albumPicasso, albumMarconi, albumKygo];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
 };
