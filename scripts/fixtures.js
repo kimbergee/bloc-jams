818ef026e609d49733a6c09b1ce92f3da419c22b
kimber <kimber.gee@gmail.com>
2016-08-29 16:27:30 -0400

    cp complete, add next and previous buttons w jquery

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+ 
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumKygo = {
+    title: 'Cloud Nine',
+    artist: 'Kygo',
+    label: 'Sony',
+    year: '2015',
+    albumArtUrl: 'assets/images/album_covers/22.png',
+    songs: [
+        { title: 'Stole the Show', duration: '2:08' },
+        { title: 'Fiction', duration: '4:03' },
+        { title: 'Firestone', duration: '4:33'},
+        { title: 'Not Alone', duration: '3:25' },
+        { title: 'Nothing Left', duration: '3:56'}
+    ]
+};
